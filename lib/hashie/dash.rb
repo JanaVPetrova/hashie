bfd93878905ea72d79a1788b00f6e9d822538e03
snop_snov <ssaweleva@gmail.com>
2013-07-10 15:45:01 +0000

    update dash

--- lib/hashie/dash.rb
+++ lib/hashie/dash.rb
@@ -1,20 +1,48 @@
-class Hashie::Dash
+class Hashie::Dash < Hash
   @properties
   @required
   @defaults
 
   def initialize(properties = {})
-  	#init
-  	raise @required.inspect
-  	raise_argument_error unless has_required_properties?(properties)
-  	properties.each do |property, value|
-  	  @properties[property] = value 
-  	end
+    init_properties(properties)
+    
+    properties.each do |property, value|
+      properties[property] = value 
+    end if properties
+    
+    self.class.defaults.each do |key, value|
+      self[key] = value
+    end
+
+    self.class.required.each do |key, value|
+      raise raise_argument_error if self[key].nil?
+    end
+
+  end
+
+  def init_properties(properties)
+    properties.each_pair do |property, value|
+      self[property] = value
+    end if properties
+  end
+
+  def raise_argument_error
+    raise "The property is required"
+  end
+
+  def self.required
+    @required
+  end
+
+  def self.defaults
+    @defaults
   end
 
   def self.property(property_name, options = {})
-  	init
+    init
+    
     @properties[property_name] = ""
+    
     if options[:required] == true
       @required.push property_name
     end
@@ -22,23 +50,27 @@ class Hashie::Dash
     unless options[:default].nil?
       @defaults[property_name] = options[:default]
     end
+
+    define_method(:"#{property_name}=") do |value|
+      self[property_name] = value
+    end
+
+    define_method(:"#{property_name}") do
+      self[property_name]
+        end
   end
 
   def self.init
-  	@properties ||= Hash.new
+    @properties ||= Hash.new
     @required ||= Array.new
     @defaults ||= Hash.new
   end
 
-  def raise_argument_error
-  	raise "The property is required"
-  end
-
-  def has_required_properties?(properties)
-    @required.each do |required_property|
-      next if properties.include? required_property
-      return false
-    end
-    true
-  end
+  #def has_required_properties?(properties)
+  #  @required.each do |required_property|
+  #    next if properties.include? required_property
+  #    return false
+  #  end
+  #  true
+  #end
 end
--- spec/hashie/dash_spec.rb
+++ spec/hashie/dash_spec.rb
@@ -5,15 +5,39 @@ describe Person do
     name = "John"
     email = "john@doe.com"
     occupation = "Rubyist"
-    #@person = Person.new(:name => name)
+    @person = Person.new(:name => name)
   end
 
   it "should raise argument error" do
     expect {Person.new}.to raise_error
   end
 
-  #it "should create new person if 'name' exists" do
-  #  @person = Person.new(:name => "Bob")
-  #  expect(@person).not_to eq nil
+  it "should create new person if 'name' exists" do
+    expect(@person).not_to eq nil
+  end
+
+  it "should return name value" do
+    expect(@person.name).to eq "John"
+  end
+
+  it "should raise argument error if set nill" do
+    expect {@person.name = nill}.to raise_error
+  end
+
+  it "should set value" do
+    @person.email = "abc@def.com"
+    expect(@person.email).to eq "abc@def.com"
+  end
+
+  it "should set default value" do
+    expect(@person.occupation).to eq  "Rubyist"
+  end
+
+  #it "should raise NoMethodError if property is not defined" do
+  #  @person[:awesome]
+  #  @person[:name] = "nill"
+  #  raise @person.inspect
+  #  expect {@person[:awesome]}.to raise_error
   #end
+
 end
