0ff68c513aab9323c3e7f99a22564ad83c664b46
snop_snov <ssaweleva@gmail.com>
2013-07-12 09:36:40 +0000

    clash start

--- lib/hashie/clash.rb
+++ lib/hashie/clash.rb
@@ -1,2 +1,34 @@
-class Hashie::Clash
+class Hashie::Clash < Hash
+
+  def initialize
+    @current_key
+  end
+
+  def method_missing(method, args = {}, &block)
+  	if method.to_s == "_end!"
+      @current_key = nil
+    elsif method =~/\!$/
+      @current_key = method.to_s.chomp("!").to_sym
+    else
+  	  @current_key = method
+      insert_value(method, args)
+    end
+    #raise self.inspect
+    self
+  end
+  
+  def insert_value(key, value)
+    #if @current_key.nil?
+    #  self[key] ||= value
+    if self[@current_key].nil?
+      self[@current_key] = value
+    else
+      self[@current_key].merge!({key => value})
+    end
+  end
+
+  def respond_to_missing?(name, include_private = false)
+    true
+  end
+
 end
--- spec/hashie/clash_spec.rb
+++ spec/hashie/clash_spec.rb
@@ -2,6 +2,33 @@ require 'spec_helper'
 
 describe Hashie::Clash do
   before(:each) do
+    @c = Hashie::Clash.new
   end
 
+  it "should add pair key-valye" do
+    @c.something("def")
+    expect(@c).to eq ({:something => "def"})
+  end
+
+  it "should have some_hash" do
+  	@c.where(:abc => 'def').order(:created_at)
+    some_hash = {:where => {:abc => 'def'}, :order => :created_at}
+  	expect(@c).to eq some_hash
+  end
+
+=begin
+  it "should set several values if method name includes !" do
+  	@c.where!.abc('def').ghi(123)._end!.order(:created_at)
+    some_hash = {:where => {:abc => 'def', :ghi => 123}, :order => :created_at}
+    raise @c.inspect
+  	expect(@c).to eq some_hash
+  end
+
+  it "should merge hashies if keys are equal" do
+  	@c.where(:abc => 'def').where(:hgi => 123)
+  	some_hash = {:where => {:abc => 'def', :hgi => 123}}
+  	raise @c.inspect
+  	expect(@c).to eq some_hash
+  end
+=end
 end
